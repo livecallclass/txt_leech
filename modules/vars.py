a1ab325b6675426223f0915935489a833a3c1085
Liveallclass <154664537+livecallclass@users.noreply.github.com>
2024-05-11 01:37:12 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,1 +1,4 @@
+API_ID = int(getenv("API_ID", "23359785"))
+API_HASH = getenv("API_HASH", "d1bb3e741e60fc0c54cbdb263684f728")
+BOT_TOKEN = getenv("BOT_TOKEN", "6738412366:AAFZnt-eNjdl6zu0gZw4lEqdrOJr7YUMGjM")
 
